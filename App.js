12f4dc2424b204f258775a33100d128e4ae557c2
alestor123 <alestoraldous@gmail.com>
2021-01-19 16:36:14 +0530

    wish

--- App.js
+++ App.js
@@ -1,2 +1,5 @@
 var chalk = require('chalk'),
-figlet = require('figlet');
+fs = require('fs'),
+figlet = require('figlet'),
+data = fs.readFileSync('./poov','utf8');
+figlet('Happy Onam',(err,data) => console.log(chalk.green(data)))
