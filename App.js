244e68d45179e91f9370297ed4bb29e3efd38f03
alestor123 <alestoraldous@gmail.com>
2021-01-19 16:46:39 +0530

    added main code

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 var chalk = require('chalk'),
 fs = require('fs'),
 figlet = require('figlet'),
-data = fs.readFileSync('./poov','utf8');
-figlet('Happy Onam',(err,data) => console.log(chalk.green(data)))
+poo = fs.readFileSync('./poov','utf8');
+figlet('Happy Onam',(err,data) => {
+console.log(chalk.green(data) + '\n')
+console.log(poo) 
+})
